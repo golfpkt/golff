bfc1c8154eea8eeaa69abbbfad24be70b52f27cd
golf <golfzakung@gmail.com>
2024-07-13 20:17:04 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,23 @@
 import streamlit as st 
 
 st.header("Hello World 👏")
-st.write("[![Star](https://avatars.githubusercontent.com/u/6429115?v=4)]")
+st.write("![Star](https://avatars.githubusercontent.com/u/6429115?v=4)")
 
 st.write("This is my first app")
 st.info("golf")
+
+
+animal = st.form('my_animal')
+
+# This is writing directly to the main body. Since the form container is
+# defined above, this will appear below everything written in the form.
+sound = st.selectbox('Sounds like', ['meow','woof','squeak','tweet'])
+
+# These methods called on the form container, so they appear inside the form.
+submit = animal.form_submit_button(f'Say it with {sound}!')
+sentence = animal.text_input('Your sentence:', 'Where\'s the tuna?')
+say_it = sentence.rstrip('.,!?') + f', {sound}!'
+if submit:
+    animal.subheader(say_it)
+else:
+    animal.subheader('&nbsp;')
