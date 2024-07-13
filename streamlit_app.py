7f2f97c4a2719b91f417cdf7390d698aa56dc9b5
golf <golfzakung@gmail.com>
2024-07-13 12:09:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st 
 
 st.header("Hello World 👏")
+st.write("[![Star](https://avatars.githubusercontent.com/u/6429115?v=4)]")
 
 st.write("This is my first app")
 st.info("golf")
