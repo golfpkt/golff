87ae0931530d81a3699b1a6931a42a96fa8f1b13
golf <golfzakung@gmail.com>
2024-07-13 11:48:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from PIL import Image
 load_css()
 
 col1, col2, col3 = st.columns(3)
-col2.image(Image.open('dp.png'))
+col2.image(Image.open('golf.jpg'))
 
 st.header('Chanin Nantasenamat, Ph.D.')
 
